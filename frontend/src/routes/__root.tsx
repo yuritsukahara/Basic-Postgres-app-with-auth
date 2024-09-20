7d56d5bdf2ee3a7375771e89ca46b401440d8cbd
yuritsukahara <yuritsukaharaoutlook.com>
2024-09-20 15:10:51 -0300

    not found page

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,6 +1,8 @@
 import { AuthContext } from '@/hooks/useAuth';
+import { Button, Typography } from '@mui/material';
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import companyLogo from '../assets/company-logo.png'
 
 type RouterContext = {
     authentication: AuthContext
@@ -8,6 +10,7 @@ type RouterContext = {
 
 export const Route = createRootRouteWithContext<RouterContext>()({
     component: Root,
+    notFoundComponent: NotFound,
 })
 
 function Root() {
@@ -15,4 +18,18 @@ function Root() {
         <Outlet />;
         <TanStackRouterDevtools />
     </>
+}
+function NotFound() {
+    return (
+        <>
+            <div className="min-h-screen flex flex-grow items-center justify-center bg-gray-50 ">
+                <div className="flex flex-col rounded-lg bg-white p-8 text-center shadow-xl gap-3">
+                    <img src={companyLogo} className='w-64' />
+                    <Typography component="h1" variant='h3'>404</Typography>
+                    <Typography component="p">Page not found</Typography>
+                    <Button href="/" variant="contained">Home</Button>
+                </div>
+            </div>
+        </ >
+    );
 }
